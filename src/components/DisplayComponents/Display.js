01a72f5430349f92ef99d149049de22c2f1164dc
hsisco <hysensisco@gmail.com>
2019-10-10 15:09:47 -0700

    Generic display

--- src/components/DisplayComponents/Display.js
+++ src/components/DisplayComponents/Display.js
@@ -1,5 +1,10 @@
 import React from "react";
 
 const Display = () => {
-  return <div className="display">{/* Display any props data here */}</div>;
-};
+  return (
+    <div className = 'display'>
+      42
+    </div>
+  )};
+
+export default Display;
